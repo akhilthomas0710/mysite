43e0fb6a7e2921c69b93f9bb01b9c906508effa9
akhilthomas0710 <akhilthomas0792@gmail.com>
2016-07-22 07:29:30 +0100

    mmmmmaa

---
+++ jscmmdemo.js
@@ -0,0 +1,33 @@
+$('#openpopup').magnificPopup({
+    items: [
+      {
+        src: "img/cmmprt/cmm1.png",
+        title: 'login page'
+      },
+	  {
+        src: "img/cmmprt/cmm2.png",
+        title: 'welcome page'
+      },
+	  {
+        src: "img/cmmprt/cmm3.png",
+        title: 'Peter & Paul fortress in SPB'
+      },
+	  {
+        src: "img/cmmprt/cmm4.png",
+        title: 'login page'
+      },
+	  {
+        src: "img/cmmprt/cmm5.png",
+        title: 'welcome page'
+      }
+	 
+	 
+	 
+	  
+	  
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
---
+++ jsdemo.js
@@ -0,0 +1,109 @@
+$('#open-popup').magnificPopup({
+    items: [
+      {
+        src: "img/mmprt/mmpic1.jpg",
+        title: 'login page'
+      },
+	  {
+        src: "img/mmprt/mmpic2.jpg",
+        title: 'welcome page'
+      },
+	  {
+        src: "img/mmprt/mmpic3.jpg",
+        title: 'Peter & Paul fortress in SPB'
+      },
+	  {
+        src: "img/mmprt/mmpic4.jpg",
+        title: 'login page'
+      },
+	  {
+        src: "img/mmprt/mmpic5.jpg",
+        title: 'welcome page'
+      },
+	  {
+        src: "img/mmprt/mmpic6.jpg",
+        title: 'Peter & Paul fortress in SPB'
+      },
+	  {
+        src: "img/mmprt/mmpic7.jpg",
+        title: 'login page'
+      },
+	  {
+        src: "img/mmprt/mmpic8.jpg",
+        title: 'welcome page'
+      },
+	  {
+        src: "img/mmprt/mmpic9.jpg",
+        title: 'Peter & Paul fortress in SPB'
+      },
+	  {
+        src: "img/mmprt/mmpic10.jpg",
+        title: 'login page'
+      },
+	  {
+        src: "img/mmprt/mmpic11.jpg",
+        title: 'welcome page'
+      },
+	  {
+        src: "img/mmprt/mmpic12.jpg",
+        title: 'Peter & Paul fortress in SPB'
+      },
+	  {
+        src: "img/mmprt/mmpic13.jpg",
+        title: 'login page'
+      },
+	  {
+        src: "img/mmprt/mmpic14.jpg",
+        title: 'welcome page'
+      },
+	  {
+        src: "img/mmprt/mmpic15.jpg",
+        title: 'Peter & Paul fortress in SPB'
+      },
+	  {
+        src: "img/mmprt/mmpic16.jpg",
+        title: 'login page'
+      },
+	  {
+        src: "img/mmprt/mmpic17.jpg",
+        title: 'welcome page'
+      },
+	  {
+        src: "img/mmprt/mmpic18.jpg",
+        title: 'Peter & Paul fortress in SPB'
+      },
+	  {
+        src: "img/mmprt/mmpic19.jpg",
+        title: 'login page'
+      },
+	  {
+        src: "img/mmprt/mmpic20.jpg",
+        title: 'welcome page'
+      },
+	  {
+        src: "img/mmprt/mmpic21.jpg",
+        title: 'Peter & Paul fortress in SPB'
+      },
+      {
+        src: "img/mmprt/mmpic22.jpg",
+        title: 'login page'
+      },
+	  {
+        src: "img/mmprt/mmpic23.jpg",
+        title: 'welcome page'
+      },
+	  {
+        src: "img/mmprt/mmpic24.jpg",
+        title: 'login page'
+      },
+	  {
+        src: "img/mmprt/mmpic25.jpg",
+        title: 'welcome page'
+      }
+	  
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
